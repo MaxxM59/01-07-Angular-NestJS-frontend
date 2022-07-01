c84a1de38fd680e6023b5537caf0f46af45166ad
MaxxM59 <87809496+MaxxM59@users.noreply.github.com>
2022-07-01 15:42:25 +0200

    Login/signup form bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderModule } from './header/header.module';
+import { LoginModule } from './auth/login/login.module';
+import { SignUpModule } from './auth/sign-up/sign-up.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HeaderModule,
+    // Auth Modules
+    LoginModule,
+    SignUpModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
